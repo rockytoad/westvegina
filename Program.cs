bb50d37808971cd2827183b374b868aa2d6fb694
roc <mario12577@hotmail.com>
2021-03-12 21:48:10 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Helpme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode;
+            double x, y, p, t;
+            Console.Write("Input the mode : ");
+            mode = Console.ReadLine();
+            Console.Write("Input x number : ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Input y number : ");
+            y = double.Parse(Console.ReadLine());
+            p = Math.Pow(x - 1, 2);
+            t = Math.Sqrt(y) + 1;
+
+            if (mode == "time" || mode == "price")
+            {
+                if (x >= 0)
+                {
+                    if (mode == "time")
+                    {
+                        Console.WriteLine("{0}, {1}", x, p);
+                    }
+                    else if (mode == "price")
+                    {
+                        Console.WriteLine("{0}, {1}", t, y);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Mode");
+            }
+        }
+    }
+}
